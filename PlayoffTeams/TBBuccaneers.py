db2d52e4ceb4f8534b61ea00ad935e99feccd06b
The Merlin Group <67569156+THEMERLINGROUP@users.noreply.github.com>
2022-01-06 12:51:04 -0500

    AB Update

--- PlayoffTeams/TBBuccaneers.py
+++ PlayoffTeams/TBBuccaneers.py
@@ -7,6 +7,11 @@ def function_tom_brady(stats):
         print(x)
 statistics = ["Yards:4990", "TD:40", "INT:12", "Passer Rating:100.5"]
 function_tom_brady(statistics)
+def antoniobrown(y):
+    return y+y
+yards = (121,17,63,124,93,101,26)
+result = map(antoniobrown, yards)
+print(list(result))
 tom_brady_bucs_vs_pats_TDs = [{'Year':2021, 'TDs':40, 'INT':12}, {'Year':2020, 'TDs':40, 'INT':12},
 {'Year':2019, 'TDs':24, 'INT':8}, {'Year':2018, 'TDs':29, 'INT':11}]
 group_TDs = groupby(tom_brady_bucs_vs_pats_TDs, key=lambda x:x['TDs'])
